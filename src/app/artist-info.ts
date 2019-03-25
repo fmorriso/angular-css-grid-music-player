6d020ab1ee6b15c915c90bc39d843cd84a8889ce
Fred Morrison <fpmorrison@verizon.net>
2019-03-24 20:52:09 -0400

    add artist

---
+++ src/app/artist-info.ts
@@ -0,0 +1,5 @@
+export interface ArtistInfo {
+  src: string;
+  caption: string;
+}
+
--- src/app/artist/artist.component.ts
+++ src/app/artist/artist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistInfo } from '../artist-info';
 
 @Component({
   selector: 'app-artist',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist.component.scss']
 })
 export class ArtistComponent implements OnInit {
+  artists: ArtistInfo[] = [
+    { src: 'r_andrea160.jpg', caption: 'Andrea Reys' },
+    { src: 'r_clara160.jpg', caption: 'Brenda Bache' },
+    { src: 'r_jen160.jpg', caption: 'Clara Stoneman' },
+    { src: 'r_jordan160.jpg', caption: 'Dory Jensen' },
+    { src: 'r_rey160.jpg', caption: 'Erica Marlay'},
+    { src: 'r_sally160.jpg', caption: 'Fanny Dylan'}
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
